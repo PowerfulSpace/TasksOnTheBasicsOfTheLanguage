8c277be08269916f6c75823855af11a134cb32e8
Alexander <sany199574@gmail.com>
2023-07-13 18:26:05 +0300

    Решил ReturnACollectionOfSquaredIntegers

--- MyTesting/Program.cs
+++ MyTesting/Program.cs
@@ -1,129 +1,14 @@
 ﻿
 
 
-IHomeCat vaska = new YardCat();
-vaska.Name = "Васька";
-CatInfoPrinter.PrintCatInfo(vaska);
-
-IHomeCat pushok = new PedigreeCat();
-pushok.Name = "Пушок";
-CatInfoPrinter.PrintCatInfo(pushok);
-
-IWildCat tiger = new Tiger();
-//CatInfoPrinter.PrintCatInfo(tiger);// ошибка
-
-
-IHomeCat testTiget = new Adapter(tiger);
-CatInfoPrinter.PrintCatInfo(testTiget);
-
-Console.ReadLine();
-
-
-class Adapter : IHomeCat
-{
-    public IWildCat WildCat { get; set; }
-    public string Name { get; set; }
-
-
-    public Adapter(IWildCat wildCat)
-    {
-        WildCat = wildCat;
-        Name = WildCat.Breed;
-    }
-
-    public void Meow()
-    {
-        WildCat.Growl();
-    }
-    public void Scratch()
-    {
-        WildCat.Scratch();
-    }
-
-}
-
-
-
-
-interface IWildCat
-{
-    string Breed { get; } // порода дикой кошки
-    void Growl();  // рычание
-    void Scratch();
-}
-
-class Tiger : IWildCat
-{
-    public string Breed { get { return "Тигр обыкновенный"; } }
-
-    public void Growl()
-    {
-        Console.WriteLine("Grrrrrrr");
-    }
-
-    public void Scratch()
-    {
-        Console.WriteLine("У меня очень острые когти, царапаюсь до смерти");
-    }
-}
 
 
 
 
 
 
+Tuple<string, string, string, int, int, int, double, Tuple<Tuple<int, int>>> tuple =
+    Tuple.Create<string, string, string, int, int, int, double, Tuple<int, int>>
+                ("Stephanie", "Alina", "Nagel", 2009, 6, 2, 1.37, Tuple.Create<int, int>(52, 3490));
 
-
-
-interface IHomeCat
-{
-    string Name { get; set; }
-    void Meow();
-    void Scratch();// царапаться
-}
-
-
-class PedigreeCat : IHomeCat
-{
-    public string Name { get; set; }
-
-    public void Meow()
-    {
-        Console.WriteLine("Урррррр уррррр");
-    }
-
-    public void Scratch()
-    {
-        Console.WriteLine("Я не царапаюсь");
-    }
-}
-
-class YardCat : IHomeCat
-{
-    public string Name { get; set; }
-
-    public void Meow()
-    {
-        Console.WriteLine("Мяу мяу!");
-    }
-
-    public void Scratch()
-    {
-        Console.WriteLine("Я царапаюсь, но не сильно");
-    }
-}
-
-class CatInfoPrinter
-{
-    public static void PrintCatInfo(IHomeCat cat)
-    {
-        Console.WriteLine("Кошачье досье:");
-        Console.WriteLine(string.Format("Имя кота: {0}", cat.Name));
-        Console.Write("Вид мяуканья: ");
-        cat.Meow();
-        Console.Write("Вид царапанья: ");
-        cat.Scratch();
-
-        Console.WriteLine();
-    }
-}
+Console.ReadKey();
---
+++ ReturnACollectionOfSquaredIntegers/Program.cs
@@ -0,0 +1,77 @@
+﻿
+//Cоздайте метод, который в качестве аргумента принимает массив целых чисел и возвращает 
+//коллекцию квадратов всех нечетных чисел массива. Для формирования коллекции 
+//используйте оператор yield.  
+
+
+
+
+
+double[] array = FillingArray(10);
+
+Show(array);
+
+var collectionOfOddNumbers = ReturnACollectionOfSquaredIntegers(array);
+var collectionOfOddNumbers2 = ReturnACollectionOfSquaredIntegers2(array);
+
+Show(collectionOfOddNumbers);
+Show(collectionOfOddNumbers2);
+
+
+
+
+Console.ReadLine();
+
+
+static IEnumerable<double> ReturnACollectionOfSquaredIntegers(double[] array)
+{
+    List<double> result = new List<double>();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            var elementInASquare = Math.Pow(array[i], 2);
+            result.Add(elementInASquare);
+        }
+    }
+
+    return result;
+}
+
+static IEnumerable<double> ReturnACollectionOfSquaredIntegers2(double[] array)
+{
+    List<double> result = new List<double>();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            var elementInASquare = Math.Pow(array[i], 2);
+            yield return elementInASquare;
+        }
+    }
+}
+
+
+static double[] FillingArray(int size)
+{
+    double[] array = new double[size];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100);
+
+    }
+    return array;
+}
+
+static void Show(IEnumerable<double> array)
+{
+    foreach (var item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
