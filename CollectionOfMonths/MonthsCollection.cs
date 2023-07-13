b7c1418268e76b4dbd1eee3302933069160b8e9e
Alexander <sany199574@gmail.com>
2023-07-13 22:59:39 +0300

    решил CollectionOfMonths

---
+++ CollectionOfMonths/MonthsCollection.cs
@@ -0,0 +1,59 @@
+﻿
+//Создайте коллекцию, в которой бы хранились наименования 12 месяцев, порядковый номер и 
+//количество дней в соответствующем месяце. Реализуйте возможность выбора месяцев, как по 
+//порядковому номеру, так и количеству дней в месяце, при этом результатом может быть не 
+//только один месяц.  
+
+
+using System;
+using System.Collections;
+using System.Globalization;
+
+class MonthsCollection : IEnumerable<Month>
+{
+    Month[] array = new Month[12];
+
+    public MonthsCollection()
+    {
+        DateTime date = DateTime.Now;
+
+        DateTimeFormatInfo dateInfo = DateTimeFormatInfo.CurrentInfo;
+
+        for (int i = 0; i <= dateInfo.MonthNames.Length - 1; i++)
+        {
+            if (string.IsNullOrEmpty(dateInfo.MonthNames[i]))
+                continue;
+
+            array[i].Name = dateInfo.MonthNames[i];
+            array[i].Number = i + 1;
+            array[i].AmountOfDays = DateTime.DaysInMonth(date.Year, i + 1);
+        }
+    }
+
+    public Month SelectMonthByNumber(int number)
+    {
+        Month month = array.FirstOrDefault(x => x.Number == number);
+        return month;
+    }
+    public Month[] selectMonthsByNumberOfDays(int days)
+    {
+        Month[] months = array.Where(x => x.AmountOfDays <= days).ToArray();
+        return months;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (var item in array)
+        {
+            yield return item;
+        }
+    }
+
+    IEnumerator<Month> IEnumerable<Month>.GetEnumerator()
+    {
+        foreach (var item in array)
+        {
+            yield return item;
+        }
+    }
+}
---
+++ CollectionOfMonths/Program.cs
@@ -0,0 +1,27 @@
+﻿//Создайте коллекцию, в которой бы хранились наименования 12 месяцев, порядковый номер и 
+//количество дней в соответствующем месяце. Реализуйте возможность выбора месяцев, как по 
+//порядковому номеру, так и количеству дней в месяце, при этом результатом может быть не 
+//только один месяц.  
+
+MonthsCollection array = new MonthsCollection();
+Show(array);
+Console.WriteLine();
+
+Console.WriteLine("Выбор месца который идёт 5 по счёту");
+Console.WriteLine(array.SelectMonthByNumber(5).Name);
+Console.WriteLine();
+
+Console.WriteLine("Выбор месцев в которых есть 30 дней");
+var selectMonthsByNumberOfDays = array.selectMonthsByNumberOfDays(30);
+Show(selectMonthsByNumberOfDays);
+
+Console.ReadLine();
+
+
+static void Show(IEnumerable<Month> array)
+{
+    foreach (Month item in array)
+    {
+        Console.WriteLine("{0,-10} Порядковый номер - {1,-10} Количество дней в месяце - {2}", item.Name, item.Number, item.AmountOfDays);
+    }
+}
