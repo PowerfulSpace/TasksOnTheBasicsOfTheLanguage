74ded88138f3b32ae3be5013d5c6d8d2949c38cd
Alexander <sany199574@gmail.com>
2023-07-21 10:00:21 +0300

    Решил сортировку по имени

--- MyTesting/Program.cs
+++ MyTesting/Program.cs
@@ -1,14 +1,1 @@
 ﻿
-
-
-
-
-
-
-
-
-Tuple<string, string, string, int, int, int, double, Tuple<Tuple<int, int>>> tuple =
-    Tuple.Create<string, string, string, int, int, int, double, Tuple<int, int>>
-                ("Stephanie", "Alina", "Nagel", 2009, 6, 2, 1.37, Tuple.Create<int, int>(52, 3490));
-
-Console.ReadKey();
--- SortByName/Program.cs
+++ SortByName/Program.cs
@@ -1,5 +1,39 @@
 ﻿
 
 
+var collect = new List<Person>
+{
+  new Person{FirstName = "Damon", LastName = "Hill"},
+  new Person{FirstName = "Niki", LastName = "Lauda"},
+  new Person{FirstName = "Ayrton", LastName = "Senna"},
+  new Person{FirstName = "Graham", LastName = "Hill"},
+  new Person{FirstName = "Damon", LastName = "Crauch"}
+};
+
+foreach (var item in collect)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\n\n\n");
+
+var sorCollect = collect.OrderBy(x => x.FirstName);
+
+foreach (var item in sorCollect)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
 
 
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("{0} {1}", FirstName, LastName);
+    }
+}
